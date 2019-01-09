19416ee757881046c6757cf03ad542ea611bcc7c
maxnawa31 <mnawa31@gmail.com>
2019-01-09 13:48:28 -0800

    working on tests for lottery contract

--- deploy.js
+++ deploy.js
@@ -9,4 +9,13 @@ const provider = new HDWalletProvider(
   'share area pattern swing picture danger busy action excite balance blouse where', 'https://ropsten.infura.io/v3/fc64655ef2534529ab07af664d5f3d5a'
 )
 
-const web3 = new Web3(provider)
+const web3 = new Web3(provider);
+
+const deploy = async () => {
+  const accounts = await web3.eth.getAccounts;
+  console.log('attempting to deploy from account', accounts[0]);
+  const result = await new web3.eth.Contract(JSON.parse(interface)).deploy({data: bytecode}).send({gas:'1000000', from: accounts[0]});
+  console.log('contract deployed');
+}
+
+deploy()
